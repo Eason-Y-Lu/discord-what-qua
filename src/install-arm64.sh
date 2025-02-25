2dc7c33250bcaac1f6d6366c8ca5a82279a4a5c9
Eason Lu <aelnosu@gmail.com>
2025-02-24 20:39:55 -0800

    Fix systemd usage

--- src/install-arm64.sh
+++ src/install-arm64.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-sudo mv what-bot-arm64.service ~/.config/systemd/user/
-sudo chmod 664 ~/.config/systemd/user/what-bot-arm64.service
-mkdir -p ~/.local/bin
-mv bot-linux-arm64 ~/.local/bin/what-bot-arm64
-sudo chmod +x ~/.local/bin/what-bot-arm64
-sudo systemctl daemon-reload
+mkdir -p ~/.config/systemd/user
+mv what-bot-arm64.service ~/.config/systemd/user/
+chmod 664 ~/.config/systemd/user/what-bot-arm64.service
+mkdir -p /usr/local/bin/what-bot
+mv bot-linux-arm64 /usr/local/bin/what-bot/what-bot-arm64
+mv token.txt /usr/local/bin/what-bot/
+chmod +x /usr/local/bin/what-bot/what-bot-arm64
+systemctl daemon-reload
 systemctl --user enable what-bot-arm64
 systemctl --user start what-bot-arm64
+systemctl --user status what-bot-arm64
--- src/install-x64.sh
+++ src/install-x64.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-mkdor -p ~/.config/systemd/user
+mkdir -p ~/.config/systemd/user
 mv what-bot-x64.service ~/.config/systemd/user/
-sudo chmod 664 ~/.config/systemd/user/what-bot-x64.service
-mkdir -p ~/.local/bin
-mv bot-linux-x64 ~/.local/bin/what-bot-x64
-sudo chmod +x ~/.local/bin/what-bot-x64
-sudo systemctl daemon-reload
+chmod 664 ~/.config/systemd/user/what-bot-x64.service
+mkdir -p /usr/local/bin/what-bot
+mv bot-linux-x64 /usr/local/bin/what-bot/what-bot-x64
+mv token.txt /usr/local/bin/what-bot/
+chmod +x /usr/local/bin/what-bot/what-bot-x64
+systemctl daemon-reload
 systemctl --user enable what-bot-x64
 systemctl --user start what-bot-x64
